9e3e899c48833487a264d2f28bdc02b41b5a4c4f
Luke <lukewebsterkhan@gmail.com>
2021-08-03 22:57:27 +0100

    commented alternate palindrome checker

--- script.js
+++ script.js
@@ -13,3 +13,14 @@ function palindrome(str) {
 }
 
 palindrome("is this a palindrome");
+
+/*function palindrome(str) {
+    let polishedString = str.replace(/\W+|_/g, "").toLowerCase();
+    let reversedString = polishedString.split("").reverse().join("").toLowerCase();
+    console.log(polishedString, reversedString)
+    if (polishedString != reversedString)
+    return false;
+    else {return true}
+  } 
+  
+  palindrome("A man, a plan, a canal. Panama");*/
