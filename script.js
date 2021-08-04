29628c4bcf5b6305f43e5253b615cfdaf1b15041
Luke <lukewebsterkhan@gmail.com>
2021-08-04 23:18:53 +0100

    added getElementById

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let checkButton = document.getElementById("input");
+
 function palindrome(str) {
   let polishedString = str.replace(/\W+|_/g, "").toLowerCase();
   let reversedString = polishedString
@@ -14,6 +16,11 @@ function palindrome(str) {
 
 palindrome("is this a palindrome");
 
+function modifyText() {
+  const notif = document.getElementById("notif");
+}
+
+addEventListener("click", modifyText);
 /*function palindrome(str) {
     let polishedString = str.replace(/\W+|_/g, "").toLowerCase();
     let reversedString = polishedString.split("").reverse().join("").toLowerCase();
