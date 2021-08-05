734d6ca2dc8a8d3820fd7d95c82d430b5af0ba05
Luke <lukewebsterkhan@gmail.com>
2021-08-06 00:11:25 +0100

    if statement added to confirm palindrome w/notif

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let checkButton = document.getElementById("input");
+var checkButton = document.getElementById("checkPalindrome");
 
 function palindrome(str) {
   let polishedString = str.replace(/\W+|_/g, "").toLowerCase();
-  let reversedString = polishedString
-    .split("")
-    .reverse()
-    .join("")
-    .toLowerCase();
+  let reversedString = polishedString.split("").reverse().join("");
 
   if (polishedString != reversedString) return false;
   else {
     return true;
   }
 }
+checkButton.addEventListener("click", function () {
+  "use strict";
 
-palindrome("is this a palindrome");
+  var value = document.getElementById("inputPalindrome").value;
+  var notification = document.getElementById("notification");
 
-function modifyText() {
-  const notif = document.getElementById("notif");
-  if (notif.firstChild.nodeValue == true) {
-    notif.firstChild.nodeValue = "Not a Palindrome";
+  if (palindrome(value)) {
+    notification.innerHTML = "That is INDEED a Palindrome young man";
+    notification.className = "alert alert-success";
+  } else {
+    notification.innerHTML = "That is NOT a Palindrome";
+    notification.className = "alert alert-failure";
   }
-}
-const el = document.getElementById('outside');
-el.addEventListener("click", modifyText);
+});
+
 /*function palindrome(str) {
     let polishedString = str.replace(/\W+|_/g, "").toLowerCase();
     let reversedString = polishedString.split("").reverse().join("").toLowerCase();
